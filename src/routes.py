e2ab4aa95eaa9d2b6b14fefcc1e92da6b4f8707b
Tudor Maiereanu <tudormaiereanu@gmail.com>
2020-07-05 23:11:52 +0200

    tunnel light

--- src/routes.py
+++ src/routes.py
@@ -1,30 +1,30 @@
 routes_data = [
     {
-        "travelTime": 34,
+        "travelTime": 1766,
         "stopsList": ["Zurich", "Montpellier", "Barcelona", "Lyon" , "Geneva"],
         "dayslist": [2,3,3,3,3],
         "emission": 473
     },
     {
-        "travelTime": 35,
+        "travelTime": 1966,
         "stopsList": ["Colmar","Avignon","Ceret","Barcelona","Mende", "Titisee-Neustadt"],
         "dayslist": [2,1,3,3,2,3],
         "emission": 519
     },
     {
-        "travelTime": 42,
+        "travelTime": 2253,
         "stopsList": ["Mayrhofen","Bozen","Digne","Girona","Barcelona","Rodez","Chamonix"],
         "dayslist": [2,2,1,2,3,2,2],
         "emission": 549
     },
     {
-        "travelTime": 46,
+        "travelTime": 2761,
         "stopsList": ["St.Moritz", "Genoa", "Marseille", "Barcelona" , "Bayonne", "Bordeaux", "Saint Etienne"],
         "dayslist": [2,2,2,3,2,2,1],
         "emission": 609
     },
     {
-        "travelTime": 40,
+        "travelTime": 2384,
         "stopsList": ["Zurich", "Milan", "Avignon", "Barcelona", "Bordeaux", "Paris", "Strasbourg"],
         "dayslist": [2,2,1,3,2,3,1],
         "emission": 625
