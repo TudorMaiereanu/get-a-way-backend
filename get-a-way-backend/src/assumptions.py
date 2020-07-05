e1394db77f7556437789ee4e774bfafa00bbc9f4
Tudor Maiereanu <tudormaiereanu@gmail.com>
2020-07-05 20:40:41 +0200

    re-estimated numbers

--- get-a-way-backend/src/assumptions.py
+++ get-a-way-backend/src/assumptions.py
@@ -1,6 +1,11 @@
 
 assumption_constants = {
-    "meetings": 10,
+    "meetings": {
+        "Metropole": 15,
+        "Stadt": 10,
+        "Land": 5,
+        "Camping": 3,
+    },
     "infection_prob": 2,
     "latent_period": 5.1, 
     "infectivity_period": 14
